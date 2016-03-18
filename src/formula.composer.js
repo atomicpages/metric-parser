beb71ca3f03a275bcd0a262485b6302e77842900
Kenneth Ceyer <kennethan@nhpcw.com>
2016-03-18 14:10:35 +0900

    Update v1.0.3.

--- src/formula.composer.js
+++ src/formula.composer.js
@@ -1,6 +1,6 @@
 /************************************************************************************************************
  *
- * @ Version 1.0.1
+ * @ Version 1.0.3
  * @ Formula Parser
  * @ Date 03. 17. 2016
  * @ Author PIGNOSE
@@ -20,15 +20,15 @@ function formulaComposer(formula) {
 formulaComposer.prototype.inArray = function(i, a) {
 	for(var idx in a) if(a[idx] == i) return idx;
 	return -1;
-}
+};
 
 formulaComposer.prototype.isOperand = function(i) {
 	return typeof i === 'object' || this.isNumeric(i);
-}
+};
 
 formulaComposer.prototype.isNumeric = function(n) {
-	return /\d+(\.\d*)?|\.\d+/.test(n);
-}
+	return (/\d+(\.\d*)?|\.\d+/).test(n);
+};
 
 formulaComposer.prototype.stringToArray = function(s) {
 	var data = [];
@@ -46,7 +46,7 @@ formulaComposer.prototype.stringToArray = function(s) {
 		}
 	}
 	return data;
-}
+};
 
 formulaComposer.prototype.layerParser = function(data, depth) {
 	var lastDepth = null;
@@ -60,13 +60,13 @@ formulaComposer.prototype.layerParser = function(data, depth) {
 					innerDepth++;
 				} else if(sub == ')') {
 					innerDepth--;
-					if(innerDepth == 0) {
+					if(innerDepth === 0) {
 						var _data = [];
 						for(var j = idx + 1; j < key; j++) {
 							_data.push(data[j]);
 						}
 						var result = this.search(_data, depth + 1);
-						if(result.result == false) {
+						if(result.result === false) {
 							return result;
 						} else {
 							data.splice(idx, key - idx + 1, result.data);
@@ -99,74 +99,102 @@ formulaComposer.prototype.layerParser = function(data, depth) {
 		result: true,
 		depth: lastDepth || depth
 	};
-}
+};
 
 formulaComposer.prototype.syntaxParser = function(data, depth, priority, lastDepth) {
-	if(data.length < 3 && ((lastDepth > 1) || (lastDepth == 1 && data.length > 1))) {
+	if((data.length < 3 && lastDepth <= 1) || (lastDepth == 1 && data.length < 1)) {
 		return {
 			result: false,
 			col: 0,
 			stack: 'syntaxParser',
 			msg: 'Formula must has characters than 3 times'
+		};
+	}
+
+	if(typeof data.length !== 'undefined') {
+		if(data.length > 1) {
+			for(var idx = 0; idx < data.length; idx++) {
+				var item = data[idx];
+				if(this.inArray(item, this.permittedOperators) == -1 && !this.isOperand(item)) {
+					return {
+						result: false,
+						col: idx,
+						stack: 'syntaxParser',
+						msg: "'" + item + "' mark is not supported."
+					};
+				}
+				if(this.inArray(item, priority) != -1) {
+					if(!this.isOperand(data[idx - 1])) {
+						return {
+							result: false,
+							col: idx - 1,
+							stack: 'syntaxParser',
+							msg: 'Left side operand is not valid.'
+						};
+					}
+
+					if(!this.isOperand(data[idx + 1])) {
+						return {
+							result: false,
+							col: idx + 1,
+							stack: 'syntaxParser',
+							msg: 'Right side operand is not valid.'
+						};
+					}
+
+					var o = {
+						operator: item,
+						operand1: data[idx - 1],
+						operand2: data[idx + 1]
+					};
+					data.splice(idx - 1, 3, o);
+					idx--;
+				} else if(this.isOperand(data[idx])) {
+					if(idx - 1 > 0) {
+						if(this.inArray(data[idx - 1], this.permittedOperators) == -1) {
+							return {
+								result: false,
+								col: idx - 1,
+								stack: 'syntaxParser',
+								msg: 'Left side operator is not valid.'
+							};
+						}
+					}
+
+					if(idx + 1 < data.length) {
+						if(this.inArray(data[idx + 1], this.permittedOperators) == -1) {
+							return {
+								result: false,
+								col: idx + 1,
+								stack: 'syntaxParser',
+								msg: 'Right side operator is not valid.'
+							};
+						}
+					}
+				}
+			}
+		} else {
+			if(lastDepth == 1) {
+				return {
+					result: true,
+					data: {
+						operator: '=',
+						operand1: data[0]
+					}
+				};
+			}
 		}
 	}
 
-	if(data.length > 2) {
-		for(var idx = 1; idx < data.length - 1; idx++) {
-			var item = data[idx];
-			if(this.inArray(item, this.permittedOperators) == -1 && !this.isOperand(item)) {
-				return {
-					result: false,
-					col: idx,
-					stack: 'syntaxParser',
-					msg: "'" + item + "' mark is not supported."
-				}
-			}
-			if(this.inArray(item, priority) != -1) {
-				if(!this.isOperand(data[idx - 1])) {
-					return {
-						result: false,
-						col: idx - 1,
-						stack: 'syntaxParser'
-					};
-				}
-
-				if(!this.isOperand(data[idx + 1])) {
-					return {
-						result: false,
-						col: idx + 1,
-						stack: 'syntaxParser'
-					};
-				}
-
-				var o = {
-					operator: item,
-					operand1: data[idx - 1],
-					operand2: data[idx + 1]
-				};
-				data.splice(idx - 1, 3, o);
-				if(data.length == 1 && typeof data[0] === 'object') {
-					data = data[0];
-				}
-				idx--;
-			} else {
-			}
-		}
-	} else {
-		return {
-			result: true,
-			data: {
-				operator: '=',
-				operand1: data[0]
-			}
-		};
+	if(data.length == 1 && typeof data[0] === 'object') {
+		data = data[0];
 	}
 
 	return {
 		result: true,
 		data: data
 	};
-}
+};
 
 formulaComposer.prototype.search = function(data, depth) {
 	if(typeof depth == 'undefined') {
@@ -177,21 +205,23 @@ formulaComposer.prototype.search = function(data, depth) {
 		data = this.stringToArray(data);
 	}
 
-	var result = this.layerParser(data, depth);
+	var result = null;
+
+	result = this.layerParser(data, depth);
 	var lastDepth = result.depth;
-	if(result.result == false) {
+	if(result.result === false) {
 		return result;
 	}
 
-	var result = this.syntaxParser(data, depth, this.primaryPriority, lastDepth);
-	if(result.result == false || lastDepth == 1) {
+	result = this.syntaxParser(data, depth, this.primaryPriority, lastDepth);
+	if(result.result === false) {
 		return result;
 	} else {
 		data = result.data;
 	}
 
-	var result = this.syntaxParser(data, depth, this.secondaryPriority, lastDepth);
-	if(result.result == false) {
+	result = this.syntaxParser(data, depth, this.secondaryPriority, lastDepth);
+	if(result.result === false) {
 		return result;
 	} else {
 		data = result.data;
@@ -201,9 +231,9 @@ formulaComposer.prototype.search = function(data, depth) {
 		result: true,
 		data: data,
 		depth: lastDepth
-	}
-}
+	};
+};
 
 formulaComposer.prototype.init = function() {
 	return this.search(this.formula);
-}
+};
